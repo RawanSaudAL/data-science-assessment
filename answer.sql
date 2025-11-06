5280a1173a9c2ebe9f5671ad9a417a07825c2097
Rawan Alahmadi <167404349+RawanSaudAL@users.noreply.github.com>
2025-11-06 11:28:24 +0300

    Add SQL query to calculate investment percentages

--- answer.sql
+++ answer.sql
@@ -1,1 +1,12 @@
--- ADD YOUR SQL QUERY HERE
+SELECT
+  it.investor_id,
+  s.sector_name,
+  ROUND(
+    100.0 * it.no_of_shares
+    / SUM(it.no_of_shares) OVER (PARTITION BY it.investor_id),
+    2
+  ) AS percentage
+FROM investor_transactions it
+JOIN sectors s
+  ON s.sector_id = it.sector_id
+ORDER BY it.investor_id, percentage DESC;
